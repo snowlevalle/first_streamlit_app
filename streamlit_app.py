e85d2a4fe2a8ab0a4510a22a79e86d6dc39eabb8
Reginald LeValle <121696503+snowlevalle@users.noreply.github.com>
2022-12-31 04:01:19 -0600

    Update streamlit_app.py
    changed the title and added an apostrophe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('''My Mom's New Healthy Dinner''')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣   Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗   Kale, Spinach & Rocket Smoothie')
